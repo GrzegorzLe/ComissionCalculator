40d0e1f414a6445e6686fce22908a14a7140fd2b
Grzegorz Leśniewski <grzegorz.lesniewski@brandpath.com>
2022-03-17 00:29:07 +0100

    minor fixes after refactoring

--- src/index.php
+++ src/index.php
@@ -1,14 +1,14 @@
 <?
 require __DIR__ . '/../vendor/autoload.php';
 
-use App/CommissionCalculator;
+use App\CommissionCalculator;
 
 $config = require_once __DIR__ . '/../config/config.php';
 
 if ( isset( $argc ) && $argc > 1 )
     $fileName = $argv[1];
 else
-    $filename = "input.txt";
+    $filename = "/app/data/input.txt";
 
 $fileContents = json_decode( "[" . str_replace( "\n", ",\n", file_get_contents( $filename ) ) . "{}]", true );
 array_pop( $fileContents );
